b349ad674923301339d95e9c942318956c60dca1
cassian <cle@tapptic.com>
2016-03-12 23:21:58 +0100

    Created cardboard scene

---
+++ Assets/Dinos&Zombies/scripts/LaserShoot.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserShoot : MonoBehaviour {
+	public GameObject explosion; 
+	private LineRenderer laser;
+
+	// Use this for initialization
+	void Start () {
+		laser = gameObject.GetComponent<LineRenderer> ();
+		laser.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire1"))
+		{
+			StopCoroutine("FireLaser");
+			StartCoroutine("FireLaser");
+		}
+	}
+
+	IEnumerator FireLaser()
+	{
+		laser.enabled = true;
+
+		while (Input.GetButton ("Fire1")) {
+			Ray ray = new Ray (transform.position, transform.forward);
+			RaycastHit hit;
+		
+			laser.SetPosition (0, ray.origin);
+
+			if (Physics.Raycast (ray, out hit, 100)) {
+				laser.SetPosition (1, hit.point);
+				if(hit.transform.gameObject.tag.Equals("Zombie") || hit.transform.gameObject.tag.Equals("Dino")) {
+					GameObject expl = Instantiate(explosion, hit.transform.position, Quaternion.identity) as GameObject;
+					Destroy(hit.transform.gameObject);
+					Destroy(expl, 3);
+				}
+
+			} else {
+				laser.SetPosition (1, ray.GetPoint(100));
+			}
+
+			yield return null;
+		}
+
+		laser.enabled = false;
+	}
+}
---
+++ Assets/Dinos&Zombies/scripts/MoveAndKill.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveAndKill : MonoBehaviour {
+	public float speed = 5F;
+	public float gravity = 20F;
+	private Vector3 moveDirection;
+	private GameObject player;
+	private Vector3 playerPos;
+	private CharacterController controller;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Hero");
+		playerPos = player.GetComponent<Transform>().position;
+		controller = gameObject.GetComponent<CharacterController> ();
+
+		if (gameObject.tag.Equals ("Dino")) {
+			transform.LookAt (2 * transform.position - playerPos);
+		}else if(gameObject.tag.Equals ("Zombie")) {
+			transform.LookAt(player.GetComponent<Transform>());
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.MoveTowards(transform.position, player.GetComponent<Transform>().position, Time.deltaTime * speed);
+		controller.Move(Vector3.down * Time.deltaTime * gravity);
+	}
+}
